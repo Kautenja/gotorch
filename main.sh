0a0722817bfcd84401ea89e764833d5bc6d4d1d0
Christian Kauten <kautencreations@gmail.com>
2023-01-19 19:50:02 -0700

    cleanup

--- main.sh
+++ main.sh
@@ -12,8 +12,6 @@
 #     build        Build the C code for the current platform
 #     test         Execute the Golang unit test suite for the project in
 #     coverage     Generate an HTML coverage report
-#     godoc        Host a documentation server at port 42768
-#     document     Generate an HTML documentation browser in the `docs` directory
 #
 
 # --- Constants --------------------------------------------------------------
