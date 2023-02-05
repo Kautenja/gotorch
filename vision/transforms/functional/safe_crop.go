4d86b195fcdf70d8e968a9aa2eb4501f006a0a2e
Christian Kauten <kautencreations@gmail.com>
2023-02-05 10:47:39 -0700

    crop method that follows torchvision semantics

---
+++ vision/transforms/functional/safe_crop.go
@@ -0,0 +1,70 @@
+// A cropping method that safely extends the image window.
+//
+// Copyright (c) 2023 Christian Kauten
+// Copyright (c) 2022 Sensory, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXTERNRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+package vision_transforms_functional
+
+import (
+    "github.com/Kautenja/gotorch"
+    F "github.com/Kautenja/gotorch/nn/functional"
+)
+
+// Crop a slice from a tensor with shape (..., H, W).
+// Crop the given image at specified location and output size. The input is a
+// Tensor with expected shape of (..., H, W). If image size is smaller than
+// output size along any edge, image is padded with 0 and then cropped.
+func SafeCrop(tensor torch.Tensor, xmin, ymin, xmax, ymax int64) torch.Tensor {
+    shape := tensor.Shape()
+    dim := len(shape)
+    if dim < 2 { panic("Crop requires inputs with 2 or more dimensions") }
+    H := shape[int64(dim - 2)]
+    W := shape[int64(dim - 1)]
+    // First check if the crop expands past the height or width of the tensor.
+    var padx1 int64 = 0
+    if xmax > W {
+        padx1 = xmax - W
+    }
+    var pady1 int64 = 0
+    if ymax > H {
+        pady1 = ymax - H
+    }
+    // Next check if the crop expands past the (x,y) origin. This is done second
+    // because it will influence the (xmax,ymax) indexes when padding the origin
+    // due to a global shift of the pixel index grid.
+    var padx0 int64 = 0
+    if xmin < 0 {
+        padx0 = -xmin
+        xmax = xmax + padx0  // correct for the index grid shift
+        xmin = 0             // Once padded, the index implicitly becomes 0
+    }
+    var pady0 int64 = 0
+    if ymin < 0 {
+        pady0 = -ymin
+        ymax = ymax + pady0  // correct for the index grid shift
+        ymin = 0             // Once padded, the index implicitly becomes 0
+    }
+    tensor = F.Pad(tensor, []int64{padx0, padx1, pady0, pady1}, F.PadConstant, 0)
+    tensor = tensor.Slice(int64(dim - 2), ymin, ymax, 1)  // t = t[..., ymin:ymax, :]
+    tensor = tensor.Slice(int64(dim - 1), xmin, xmax, 1)  // t = t[..., :, xmin:xmax]
+    return tensor
+}
---
+++ vision/transforms/functional/safe_crop_test.go
@@ -0,0 +1,152 @@
+// test cases for crop.go
+//
+// Copyright (c) 2023 Christian Kauten
+// Copyright (c) 2022 Sensory, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXTERNRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+package vision_transforms_functional_test
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+    "github.com/Kautenja/gotorch"
+    "github.com/Kautenja/gotorch/vision/transforms/functional"
+)
+
+func TestSafeCropIdentity(t *testing.T) {
+    tensor := torch.NewTensor([][]float32{
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 8, 7},
+        {6, 5, 4, 3},
+    })
+    output := vision_transforms_functional.SafeCrop(tensor, 0, 0, 4, 4)
+    assert.True(t, output.Equal(tensor))
+}
+
+func TestSafeCropExpandsBounds(t *testing.T) {
+    tensor := torch.NewTensor([][]float32{
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 8, 7},
+        {6, 5, 4, 3},
+    })
+    output := vision_transforms_functional.SafeCrop(tensor, -1, -1, 5, 5)
+    expected := torch.NewTensor([][]float32{
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 2, 3, 0},
+        {0, 4, 5, 6, 7, 0},
+        {0, 8, 9, 8, 7, 0},
+        {0, 6, 5, 4, 3, 0},
+        {0, 0, 0, 0, 0, 0},
+    })
+    assert.True(t, expected.Equal(output))
+}
+
+func TestSafeCropExpandsBoundsFromOrigin(t *testing.T) {
+    tensor := torch.NewTensor([][]float32{
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 8, 7},
+        {6, 5, 4, 3},
+    })
+    output := vision_transforms_functional.SafeCrop(tensor, 0, 0, 5, 5)
+    expected := torch.NewTensor([][]float32{
+        {0, 1, 2, 3, 0},
+        {4, 5, 6, 7, 0},
+        {8, 9, 8, 7, 0},
+        {6, 5, 4, 3, 0},
+        {0, 0, 0, 0, 0},
+    })
+    assert.True(t, expected.Equal(output))
+}
+
+func TestSafeCropExpandsBoundsFromWindow(t *testing.T) {
+    tensor := torch.NewTensor([][]float32{
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 8, 7},
+        {6, 5, 4, 3},
+    })
+    output := vision_transforms_functional.SafeCrop(tensor, -1, -1, 4, 4)
+    expected := torch.NewTensor([][]float32{
+        {0, 0, 0, 0, 0},
+        {0, 0, 1, 2, 3},
+        {0, 4, 5, 6, 7},
+        {0, 8, 9, 8, 7},
+        {0, 6, 5, 4, 3},
+    })
+    assert.True(t, expected.Equal(output))
+}
+
+func TestSafeCropFromOrigin(t *testing.T) {
+    tensor := torch.NewTensor([][]float32{
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 8, 7},
+        {6, 5, 4, 3},
+    })
+    expected := torch.NewTensor([][]float32{
+        {0, 1},
+        {4, 5},
+        {8, 9},
+    })
+    output := vision_transforms_functional.SafeCrop(tensor, 0, 0, 2, 3)
+    assert.True(t, output.Equal(expected), "got %v, expected %v", output, expected)
+}
+
+func TestSafeCropToBorder(t *testing.T) {
+    tensor := torch.NewTensor([][]float32{
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 8, 7},
+        {6, 5, 4, 3},
+    })
+    expected := torch.NewTensor([][]float32{
+        {6, 7},
+        {8, 7},
+        {4, 3},
+    })
+    output := vision_transforms_functional.SafeCrop(tensor, 2, 1, 4, 4)
+    assert.True(t, output.Equal(expected), "got %v, expected %v", output, expected)
+}
+
+func TestSafeCropArbitrarySafeWindow(t *testing.T) {
+    tensor := torch.NewTensor([][]float32{
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 8, 7},
+        {6, 5, 4, 3},
+    })
+    expected := torch.NewTensor([][]float32{
+        {5, 6},
+        {9, 8},
+    })
+    output := vision_transforms_functional.SafeCrop(tensor, 1, 1, 3, 3)
+    assert.True(t, output.Equal(expected), "got %v, expected %v", output, expected)
+}
+
+func TestSafeCropTransformPanicsOn1DimensionalInput(t *testing.T) {
+    tensor := torch.NewTensor([]float32{0, 1, 2, 3})
+    assert.PanicsWithValue(t, "Crop requires inputs with 2 or more dimensions", func() {
+        vision_transforms_functional.SafeCrop(tensor, 0, 0, 1, 1)
+    })
+}
